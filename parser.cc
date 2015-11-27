19052ce152fef113cce3ec8d5c0856b789b10126
At0m0s <At0m0s@users.noreply.github.com>
2015-11-27 18:22:20 -0500

    Update parser.cc

--- parser.cc
+++ parser.cc
@@ -1,26 +1,37 @@
 /*
- * parser.cxx
+ * parser.cc
  *
  *  Created on: Nov 21, 2015
  *      Author: At0m0s
  */
-
+ 
 #include <iostream>
 #include <fstream>
 #include <iomanip>
 #include <vector>
 #include <string>
 #include <boost/algorithm/string.hpp>
+#include <sstream>
 
 using namespace std;
 using namespace boost;
 
 int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        cout << "Usage: " << argv[0] << " <delimeter> <input file> <output file>" << endl;
+    if (argc != 6) {
+        cout << "Usage: " << argv[0] << " <delimeter> <input file> <output file> <userIndex> <passIndex>" << endl;
         return 1;
     }
-    cout << "Welcome to the database parser, created by At0m0s." << endl;
+
+    istringstream us(argv[5]);
+    istringstream ps(argv[6]);
+    int userIndex;
+    int passIndex;
+    if (!(us >> userIndex))
+        cerr << "Invalid number " << argv[5] << endl;
+    if(!(ps >> passIndex))
+        cerr << "Invalid number " << argv[6] << endl;
+    
+    cout << "Welcome to the combo parser, created by At0m0s." << endl;
     vector <string> fields;
     string line;
     string delimeter = argv[1];
@@ -31,16 +42,16 @@ int main(int argc, char* argv[]) {
         cout << "Parsing file";
         while ( getline (inFile,line) )
         {
-            size_t n = std::count(line.begin(), line.end(), ':');
+            size_t n = std::count(line.begin(), line.end(), '_');
             if (line.find(delimeter) != std::string::npos) {
-                if (line.find(";") != std::string::npos || line.find("#") != std::string::npos || line.find("&#") != std::string::npos || n != 3) {
+                if (line.find(";") != std::string::npos || line.find("#") != std::string::npos || line.find("&#") != std::string::npos) {
                     //break;
                 }
                 else {
                     //cout << line << endl;
                     split( fields, line, is_any_of( delimeter ) );
                     cout << ".";
-                    outFile << fields[1] << ":" << fields[3] << endl;
+                    outFile << fields[userIndex] << ":" << fields[passIndex] << endl;
                 }
             }
         }
